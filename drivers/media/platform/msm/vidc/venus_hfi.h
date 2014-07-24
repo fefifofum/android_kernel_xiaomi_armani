e1c6e7cdcc9cc4c0b0b7cb19387d183bd6c19166
Maheshwar Ajja <majja@codeaurora.org>
2014-07-24 13:43:09 +0530

    msm: vidc: Disable clock gating feature
    Clock gating between inter frames when video hardware
    is in IDLE state is not giving any power savings and
    at the same time this feature is resulting in few
    stability issues. So disabling clock gating feature.
    
    Change-Id: I3d1d4521ec3ee2fbd25d80b33bd5c531a25d9034
    Signed-off-by: Maheshwar Ajja <majja@codeaurora.org>

--- drivers/media/platform/msm/vidc/venus_hfi.h
+++ drivers/media/platform/msm/vidc/venus_hfi.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  * Copyright (C) 2015 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -152,6 +152,7 @@ struct hal_data {
 };
 
 enum vidc_clocks {
+	VCODEC_NONE,
 	VCODEC_CLK,
 	VCODEC_AHB_CLK,
 	VCODEC_AXI_CLK,
